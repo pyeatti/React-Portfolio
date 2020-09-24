3990403d8f7c73775a4b965a582961814a4b4c23
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-24 18:46:38 -0500

    Updates

---
+++ precache-manifest.70f953abfaa9f6126a091fa8274cc6d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb84606d4f0e79878fc88e2809710d33",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "b488c5483352ba76aeb6",
+    "url": "/React-Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b2a4cc5789d543e94a45",
+    "url": "/React-Portfolio/static/css/main.1812bbce.chunk.css"
+  },
+  {
+    "revision": "b488c5483352ba76aeb6",
+    "url": "/React-Portfolio/static/js/2.a4aee0f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.a4aee0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a4cc5789d543e94a45",
+    "url": "/React-Portfolio/static/js/main.ac8b4765.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  },
+  {
+    "revision": "e94901cefef0204f4d7daafc74723450",
+    "url": "/React-Portfolio/static/media/budgetapp.e94901ce.png"
+  },
+  {
+    "revision": "2f511205601a564a32be6aa68d0eae37",
+    "url": "/React-Portfolio/static/media/burgerlogger.2f511205.png"
+  },
+  {
+    "revision": "739d7501d1cec0ee71a293035df7a12e",
+    "url": "/React-Portfolio/static/media/copyslider.739d7501.png"
+  },
+  {
+    "revision": "77ddcfb145d8ee419ddf4153e6d73215",
+    "url": "/React-Portfolio/static/media/fitnesstracker.77ddcfb1.png"
+  },
+  {
+    "revision": "f4f49cf00ddb024c8c3b2f9b13b1c902",
+    "url": "/React-Portfolio/static/media/passwordgenerator.f4f49cf0.png"
+  },
+  {
+    "revision": "23b5653ffcb6d92eb12425aec3dc14f8",
+    "url": "/React-Portfolio/static/media/profilepic.23b5653f.jpg"
+  },
+  {
+    "revision": "7327f6fec07adcbfc4ccff4807e7100a",
+    "url": "/React-Portfolio/static/media/project1.7327f6fe.png"
+  },
+  {
+    "revision": "f36aa578c46dac4ecfa1cb6031f0375a",
+    "url": "/React-Portfolio/static/media/project2.f36aa578.png"
+  },
+  {
+    "revision": "9dab6f2962c6ce1a81a7f2d637785231",
+    "url": "/React-Portfolio/static/media/stoneguardians.9dab6f29.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.fe17eff5f1c54a586f507552b4cee286.js"
+  "/React-Portfolio/precache-manifest.70f953abfaa9f6126a091fa8274cc6d3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
