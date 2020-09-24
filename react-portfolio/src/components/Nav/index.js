1c81d962a9f43b3508eb1d003195112b6ffc9c09
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-24 18:59:19 -0500

    testing router

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,58 +1,61 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
-      <a
-        className="navbar-brand text-white font-weight-bold float-right"
-        href="/about"
-      >
-        IAN PYEATT
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link text-light font-weight-bold" href="/about">
-              About
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link font-weight-bold text-light"
-              href="/portfolio"
-            >
-              Portfolio
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link font-weight-bold text-light"
-              href="mailto:pyeatt08@gmail.com"
-            >
-              Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a
-              className="nav-link font-weight-bold text-light"
-              href="./assets/IP_Resume.pdf"
-            >
-              Resumé
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent">
+        <a
+          className="navbar-brand text-white font-weight-bold float-right"
+          href="/about"
+        >
+          IAN PYEATT
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link text-light font-weight-bold" href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link font-weight-bold text-light"
+                href="/portfolio"
+              >
+                Portfolio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link font-weight-bold text-light"
+                href="mailto:pyeatt08@gmail.com"
+              >
+                Contact
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link font-weight-bold text-light"
+                href="./assets/IP_Resume.pdf"
+              >
+                Resumé
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Router>
   );
 }
