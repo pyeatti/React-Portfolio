2302a02878c048a446bd56ef0967306b9c658d6d
Ian Pyeatt <pyeatt08@gmail.com>
2020-09-24 18:43:07 -0500

    Updates

---
+++ precache-manifest.fe17eff5f1c54a586f507552b4cee286.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d996e23a5e70a8fda84282846fdfd94",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "b488c5483352ba76aeb6",
+    "url": "/react-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "769026ad3418a0923a6f",
+    "url": "/react-portfolio/static/css/main.1812bbce.chunk.css"
+  },
+  {
+    "revision": "b488c5483352ba76aeb6",
+    "url": "/react-portfolio/static/js/2.a4aee0f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.a4aee0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769026ad3418a0923a6f",
+    "url": "/react-portfolio/static/js/main.e2c20705.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "e94901cefef0204f4d7daafc74723450",
+    "url": "/react-portfolio/static/media/budgetapp.e94901ce.png"
+  },
+  {
+    "revision": "2f511205601a564a32be6aa68d0eae37",
+    "url": "/react-portfolio/static/media/burgerlogger.2f511205.png"
+  },
+  {
+    "revision": "739d7501d1cec0ee71a293035df7a12e",
+    "url": "/react-portfolio/static/media/copyslider.739d7501.png"
+  },
+  {
+    "revision": "77ddcfb145d8ee419ddf4153e6d73215",
+    "url": "/react-portfolio/static/media/fitnesstracker.77ddcfb1.png"
+  },
+  {
+    "revision": "f4f49cf00ddb024c8c3b2f9b13b1c902",
+    "url": "/react-portfolio/static/media/passwordgenerator.f4f49cf0.png"
+  },
+  {
+    "revision": "23b5653ffcb6d92eb12425aec3dc14f8",
+    "url": "/react-portfolio/static/media/profilepic.23b5653f.jpg"
+  },
+  {
+    "revision": "7327f6fec07adcbfc4ccff4807e7100a",
+    "url": "/react-portfolio/static/media/project1.7327f6fe.png"
+  },
+  {
+    "revision": "f36aa578c46dac4ecfa1cb6031f0375a",
+    "url": "/react-portfolio/static/media/project2.f36aa578.png"
+  },
+  {
+    "revision": "9dab6f2962c6ce1a81a7f2d637785231",
+    "url": "/react-portfolio/static/media/stoneguardians.9dab6f29.png"
+  }
+]);
